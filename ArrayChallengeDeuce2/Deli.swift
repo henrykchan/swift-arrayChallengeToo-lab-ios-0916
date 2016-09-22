48b3b531c9fb651cce92ae263cac6a6d05f966e5
henrykchan <chan.henryk@gmail.com>
2016-09-22 15:39:24 -0400

    ArrayChallenge2 Complete

--- ArrayChallengeDeuce2/Deli.swift
+++ ArrayChallengeDeuce2/Deli.swift
@@ -16,24 +16,88 @@ class Deli {
     // 1
     func addNameToLine(name: String) -> String {
                 
-       // TODO: Implement this function.
+        
+        
+        //Adding Billy and Meg to the front of the line. If anyone else, just append to the line
+        switch name {
+        case "Billy Crystal":
+            line.insert("Billy Crystal", at: 0)
+        
+        case "Meg Ryan" :
+            line.insert("Meg Ryan", at: 0)
+            
+        default:
+            line.append(name)
+        }
+        
+        
+        //Greetings
+
+        
+        if line.count == 1 {
+            
+            return "Welcome \(name), you're first in line!"
+        }
+        
+        else if name == "Billy Crystal" {
+            
+            return "Welcome Billy Crystal! You can sit wherever you like."
+        }
+        
+        else if name == "Meg Ryan" {
+            
+            return "Welcome Meg Ryan! You can sit wherever you like."
+        }
+        
+        else {
+            
+            return "Welcome \(name), you're number \(line.count) in line."
+        }
+        
+
         
     }
-    
+
     
     // 2
     func nowServing() -> String {
         
-        // TODO: Implement this function.
-
+        if line == [] {
+            
+            return "There is no one to be served."
+        }
+        
+        else {
+            
+            return "Now serving \(line.remove(at: 0))!"
+        }
     }
+
+    
     
     
     // 3
     func lineDescription() -> String {
         
-        // TODO: Implement this function.
-
+        var numberAndName = "The line is:"
+        
+        if line.isEmpty {
+            
+            return "The line is currently empty."
+        }
+        
+        else {
+            
+            for (index, eachName) in line.enumerated() {
+                
+                let numbering = index + 1
+                
+                numberAndName = numberAndName + ("\n\(numbering). \(eachName)")
+            }
+            
+        }
+        
+        return numberAndName
     }
     
 }
